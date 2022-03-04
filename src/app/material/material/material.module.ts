d68c15fecd2311c3fe9d7ae0f1d6b84f26ef1eee
andreylpereira <andreylpereira@gmail.com>
2022-03-03 23:21:54 -0300

    Organização de pastas, arquivos, inserção de angular material e bootstrap

---
+++ src/app/material/material/material.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+
+/* imports material */
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatTableModule} from '@angular/material/table';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatGridListModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatListModule,
+    MatDialogModule,
+    MatTableModule,
+    MatExpansionModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatGridListModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatListModule,
+    MatDialogModule,
+    MatTableModule,
+    MatExpansionModule
+  ]
+})
+
+export class MaterialModule {
+}
