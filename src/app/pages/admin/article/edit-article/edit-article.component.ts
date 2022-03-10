9199d41f7ebe3c22e2fc6e375ce8cd92c0dd0719
andreylpereira <andreylpereira@gmail.com>
2022-03-10 19:53:29 -0300

    Funcionalidade de publicar artigo funcional

--- src/app/pages/admin/article/edit-article/edit-article.component.ts
+++ src/app/pages/admin/article/edit-article/edit-article.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import Categories from 'src/app/models/categories/categories.model';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Article from '../../../../models/Article/article.model';
+import { LoginService } from 'src/app/services/login/login.service';
+import { ArticlesService } from 'src/app/services/articles/articles.service';
 
 @Component({
   selector: 'app-edit-article',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-article.component.scss']
 })
 export class EditArticleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public listCategories: Categories[] = [];
+  constructor(
+    private categoriesService: CategoriesService,
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private articlesService: ArticlesService
+  ) {
+    this.categoriesService.getCategories().subscribe((data: any) => {
+      this.listCategories = data;
+    });
   }
 
+  ngOnInit(): void {}
+
+  articleForm: FormGroup = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    body: ['', [Validators.required]],
+    category: [0, [Validators.required]],
+  });
+
+  createArticle() {
+    let user = this.loginService.getUser();
+    let author = `${user.firstName} ${user.lastName}`;
+
+    let article: Article = {
+      title: this.articleForm.get('title')?.value,
+      body: this.articleForm.get('body')?.value,
+      category: this.articleForm.get('category')?.value,
+      author: author,
+    };
+
+
+  }
 }
--- src/app/pages/admin/article/form-article/form-article.component.ts
+++ src/app/pages/admin/article/form-article/form-article.component.ts
@@ -1,27 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import Categories from 'src/app/models/categories/categories.model';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Article from '../../../../models/Article/article.model';
+import { LoginService } from 'src/app/services/login/login.service';
+import { ArticlesService } from 'src/app/services/articles/articles.service';
 
 @Component({
   selector: 'app-form-article',
   templateUrl: './form-article.component.html',
-  styleUrls: ['./form-article.component.scss']
+  styleUrls: ['./form-article.component.scss'],
 })
 export class FormArticleComponent implements OnInit {
-
-  public listCategories: Categories[] = []
-  constructor(private categoriesService: CategoriesService) {
-    this.categoriesService.getCategories().subscribe((data:any) => {
-
-      this.listCategories = data
+  public listCategories: Categories[] = [];
+  constructor(
+    private categoriesService: CategoriesService,
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private articlesService: ArticlesService
+  ) {
+    this.categoriesService.getCategories().subscribe((data: any) => {
+      this.listCategories = data;
     });
-   }
-
-  ngOnInit(): void {
   }
 
-  verLista() {
-    console.log(this.listCategories);
+  ngOnInit(): void {}
 
+  articleForm: FormGroup = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    body: ['', [Validators.required]],
+    category: [0, [Validators.required]],
+  });
+
+  createArticle() {
+    let user = this.loginService.getUser();
+    let author = `${user.firstName} ${user.lastName}`;
+
+    let article: Article = {
+      title: this.articleForm.get('title')?.value,
+      body: this.articleForm.get('body')?.value,
+      category: this.articleForm.get('category')?.value,
+      author: author,
+    };
+
+    this.articlesService.createArticle(article);
   }
 }
